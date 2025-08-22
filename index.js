d8015822df1a99744cf6d903b16c94e12e229203
LeoTchies <leo.tolotti.chies@gmail.com>
2025-08-22 09:09:03 -0300

    Upload do projeto
    Projeto inicial de back-end, com estrutura de acesso à uma site

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/public/index.html")});
+
+app.get("/secret", (req,res) => {
+    res.sendFile(__dirname + "/public/secret.html")});   
+    
+app.post("/check", (req, res) => {
+  const password = req.body.password;
+
+  if (password === "novidades") {   
+    res.redirect("/secret");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+}); 
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
